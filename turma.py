a63190c4ddcbe5495a1afb8cf364fbd73163cd59
MIGUELPC\Miguel Peres <miperesbrandt@gmail.com>
2024-06-01 15:45:38 -0300

    Reestruturando para modularização

--- turma.py
+++ turma.py
@@ -1,9 +1,17 @@
 import os
 
-DATA_PATH = "data_turma"
-ID_FILE_PATH = os.path.join(DATA_PATH, "proximo_id.txt")
+# Declarando as funções de acesso
+__all__ = []
 
-def gerar_novo_id() -> int:
+# Globais
+_turmas: list[dict] = []
+
+# Constantes
+_DATA_PATH: str = "data_turma"
+_ID_FILE_PATH: str = os.path.join(_DATA_PATH, "proximo_id.txt")
+
+# Funções internas
+def gera_novo_id() -> int:
     """
     Gera sequencialmente um novo ID único, para uma nova instância de dicionário
 
@@ -11,18 +19,31 @@ def gerar_novo_id() -> int:
 
     Cria os arquivos necessários caso não existam: o diretório DATA_PATH e o arquivo ID_FILE_PATH
     """
-    if not os.path.exists(DATA_PATH):
-        os.makedirs(DATA_PATH)
+    if not os.path.exists(_DATA_PATH):
+        os.makedirs(_DATA_PATH)
     
-    if not os.path.exists(ID_FILE_PATH):
+    if not os.path.exists(_ID_FILE_PATH):
         id_atual = 1
     else:
-        with open(ID_FILE_PATH, 'r') as file:
+        with open(_ID_FILE_PATH, 'r') as file:
             id_atual = int(file.read())
 
     id_proximo = id_atual + 1
 
-    with open(ID_FILE_PATH, 'w') as file:
+    with open(_ID_FILE_PATH, 'w') as file:
             file.write(str(id_proximo))
     
     return id_atual
+
+def read_turmas() -> None:
+    raise NotImplementedError()
+
+def write_turmas() -> None:
+    raise NotImplementedError()
+
+
+# Isso executa quando turma.py é executado diretamente, e não quando importado
+if __name__ == "__main__":
+    # Salvar turmas ao final do programa
+    import atexit
+    atexit.register(write_turmas)
